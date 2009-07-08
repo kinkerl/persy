95cedf239dce1a5b365f2f790fd1a0d69c56adf0
Dennis Schwertel <s@digitalkultur.net>
2009-07-08 12:36:21 +0200

    a lot of cleanup

--- usr/lib/persy/persy.py
+++ usr/lib/persy/persy.py
@@ -14,16 +14,24 @@ import paramiko
 __author__ = "Dennis Schwertel"
 __copyright__ = "Copyright (C) 2009 Dennis Schwertel"
 
-USERHOME_FOLDER = '.persy'
-USERHOME_GITREPO = '.git'
 GIT = '/usr/bin/git'
 USERHOME = os.environ["HOME"]
+USERHOME_FOLDER = '.persy'
+USERHOME_GITREPO = '.git'
+CONFIGFILE='config'
 
-#flags for the filesystem events we want to watch out for
-FLAGS=EventsCodes.ALL_FLAGS
-mask = FLAGS['IN_MODIFY'] | FLAGS['IN_DELETE_SELF']|FLAGS['IN_DELETE'] | FLAGS['IN_CREATE'] | FLAGS['IN_CLOSE_WRITE'] | FLAGS['IN_MOVE_SELF'] | FLAGS['IN_MOVED_TO'] | FLAGS['IN_MOVED_FROM'] # watched events
 
 
+DEFAULT_CONFIG="""[local]
+sleep = 5
+watched = 
+
+[remote]
+use_remote = False
+sleep = 30
+hostname = 
+path = 
+"""
 
 #init logging
 log = logging.getLogger("")
@@ -35,6 +43,7 @@ fmt = logging.Formatter("%(asctime)s %(levelname)-5s %(message)s", "%x %X")
 hdlr.setFormatter(fmt)
 log.addHandler(hdlr)
 log.setLevel(logging.INFO) #set verbosity to show all messages of severity >= DEBUG
+
 
 dome = True
 lastevent= time.time()
@@ -147,16 +156,18 @@ class TheSyncer(Thread):
 					os.popen("%s add %s"%(GIT, watch))
 				if WATCHED:
 					os.popen("%s commit -am \"Backup by me\""%GIT)
+					if config['remote']['use_remote']:
+						os.popen("%s push origin master"%GIT)
 					os.popen("%s gc"%GIT)
 
 			#autopull and push updates every x secs
-			if USE_REMOTE and tick >= (self.sleep_remote/self.sleep_local) :
+			if config['remote']['use_remote'] and tick >= (self.sleep_remote/self.sleep_local) :
 				tick = 0
 				log.info("remote sync")
 				os.chdir(USERHOME)
 				if WATCHED:
 					os.popen("%s pull origin master"%GIT)
-					os.popen("%s push origin master"%GIT)
+					#os.popen("%s push origin master"%GIT)
 
 
 def initLocal():
@@ -172,6 +183,7 @@ def initLocal():
 	os.popen("%s config user.email \"%s\""%(GIT,config['general']['useremail']))
 
 def initRemote():
+	'''initialises the remote repository'''
 	if not config['remote']['hostname'] or not config['remote']['path']:
 		log.critical("no hostname or remote path set, cant init remote server")
 		return
@@ -187,6 +199,7 @@ def initRemote():
 		config.write()
 
 def syncWithRemote():
+	'''Syncs with a remote server'''
 	if not config['remote']['hostname'] or not config['remote']['path']:
 		log.critical("no hostname or remote path set, cant add remote server")
 	else:
@@ -203,9 +216,14 @@ def runLocal():
 	'''The normal syncer'''
 	InterruptWatcher()
 
+	#flags for the filesystem events we want to watch out for
+	FLAGS=EventsCodes.ALL_FLAGS
+	mask = FLAGS['IN_MODIFY'] | FLAGS['IN_DELETE_SELF']|FLAGS['IN_DELETE'] | FLAGS['IN_CREATE'] | FLAGS['IN_CLOSE_WRITE'] | FLAGS['IN_MOVE_SELF'] | FLAGS['IN_MOVED_TO'] | FLAGS['IN_MOVED_FROM'] # watched events
+
 	wm = WatchManager()
 	notifier = Notifier(wm, FileChangeHandler())
 
+	#addin the watched directories
 	for watch in WATCHED:
 		wdd = wm.add_watch("%s"%(watch), mask, rec=True)
 
@@ -216,28 +234,33 @@ def runLocal():
 	tester.start()
 	notifier.loop()
 
-if __name__ == "__main__":
-
+def main(argv):
+	args = argv[1:]
+	#cli options
 	from optparse import OptionParser
 	parser = OptionParser()
 	parser.add_option("--init",action="store_true", default=False, help="initializes the local repository")
 	parser.add_option("--initremote",action="store_true", default=False, help="initializes the remote repository")
 	parser.add_option("--syncwithremote",action="store_true", default=False, help="syncs with a remote repository")
+	parser.add_option("--dry",action="store_true", default=False, help="no real git actions")
 	parser.add_option("--config",action="store_true", default=False, help="needed to change configurations")
 	parser.add_option("--path", dest="path", default="", help="path on the server")
 	parser.add_option("--hostname", dest="hostname", default="", help="hostname of the remote server")
 	parser.add_option("--add_dir", dest="add_dir", default="", help="add local wachted folders")
-	parser.add_option("--username", dest="username", default="", help="sets username")
-	parser.add_option("--useremail", dest="useremail", default="", help="sets useremail")
-	(options, args) = parser.parse_args()
-	#configuration
+	(options, args) = parser.parse_args(args)
+
+
+	#create programdirectory and a default config file
+	if not os.path.exists(os.path.join(USERHOME,USERHOME_FOLDER)):
+		os.makedirs(os.path.join(USERHOME,USERHOME_FOLDER))
+	if not os.path.exists(os.path.join(USERHOME,USERHOME_FOLDER,CONFIGFILE)):
+		with open(os.path.join(USERHOME,USERHOME_FOLDER,CONFIGFILE), "w+") as f:
+			f.write(DEFAULT_CONFIG)
+
+	#load and set configuration
 	global config
-	#create programdirectory
-	if not os.path.isdir("%s/%s"%(USERHOME,USERHOME_FOLDER)):
-		os.popen("mkdir %s/%s"%(USERHOME,USERHOME_FOLDER))
 	config = ConfigObj("%s/%s/config"%(USERHOME,USERHOME_FOLDER))
-	
-	USE_REMOTE = config['remote']['use_remote']=='True'
+	config['remote']['use_remote'] = config['remote']['use_remote']=='True'
 	config['local']['sleep'] = int(config['local']['sleep']or 5) #5 is default
 	config['remote']['sleep'] = int(config['remote']['sleep']or 30) #30 is default
 
@@ -246,12 +269,15 @@ if __name__ == "__main__":
 			config['remote']['hostname'] = options.hostname
 		if options.path:
 			config['remote']['path'] = options.path
-		if options.useremail:
-			config['local']['useremail'] = options.useremail
-		if options.username:
-			config['local']['username'] = options.username
 		if options.add_dir:
-			config['local']['watched'].append(options.add_dir)
+			if type(config['local']['watched']) is str:
+				if config['local']['watched']:
+					config['local']['watched'] = (config['local']['watched'], options.add_dir)
+				else:
+					config['local']['watched'] = (options.add_dir)
+			else:
+				config['local']['watched'].append(options.add_dir)
+		print "WRITE"
 		config.write()
 		sys.exit(0)
 	elif options.syncwithremote or options.initremote:
@@ -268,6 +294,7 @@ if __name__ == "__main__":
 
 	WATCHED = config['local']['watched']
 
+
 	if options.init:
 		initLocal()
 	elif options.initremote:
@@ -280,5 +307,12 @@ if __name__ == "__main__":
 			sys.exit(-1)
 		runLocal()
 
+
+if __name__ == '__main__':
+	try:
+		main(sys.argv)
+	except Exception:
+		logging.root_logger.exception('Unexpected error')
+		raise
 
 
