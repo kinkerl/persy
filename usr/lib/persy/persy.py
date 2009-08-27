2cc0661d71142b5f44e5cb8b0422d224dd467d44
Dennis Schwertel <s@digitalkultur.net>
2009-08-27 11:08:47 +0200

    small cwd bugfix AND reinit of pyinotify to prevent freezing

--- usr/lib/persy/persy.py
+++ usr/lib/persy/persy.py
@@ -186,6 +186,7 @@ executing the local commits, the remote pulls/pushs and the updating of the igno
 		self.lastcommit = 0
 		self.lastsync = 0
 		self.lastignore = 0
+		self.lastrenotify = time.time()
 		self.running = True
 
 	def generateCommitMessage(self):
@@ -267,6 +268,19 @@ executing the local commits, the remote pulls/pushs and the updating of the igno
 					gitignore()
 				except Exception as e:
 					log.warn(str(e))
+			#restarts tne notify hook, it hangs for no apparent reason
+			if time.time() - self.lastrenotify > 60:
+				self.lastrenotify = time.time()
+				if notifier:
+					try:
+						notifier.stop()
+					except RuntimeError:
+						pass
+					except OSError:
+						pass
+					except KeyError:
+						pass
+					notify_init()
 
 
 class Talker:
@@ -652,12 +666,10 @@ def optimize():
 
 
 
-def persy_start():
-	''' Starts Persy'''
-	global worker
-	global notifier
-	log.info("start working")
 
+def notify_init():
+	log.info("inits notify hook")
+	global notifier
 	FLAGS=EventsCodes.ALL_FLAGS
 	mask = FLAGS['IN_MODIFY'] | FLAGS['IN_DELETE_SELF']|FLAGS['IN_DELETE'] | FLAGS['IN_CREATE'] | FLAGS['IN_CLOSE_WRITE'] | FLAGS['IN_MOVE_SELF'] | FLAGS['IN_MOVED_TO'] | FLAGS['IN_MOVED_FROM'] # watched events
 
@@ -665,13 +677,21 @@ def persy_start():
 	#addin the watched directories
 	for watch in config['local']['watched']:
 		wdd = wm.add_watch("%s"%(watch), mask, rec=True)
-
-	worker = TheSyncer(config['remote']['sleep'], config['local']['sleep'])
 	notifier = ThreadedNotifier(wm, FileChangeHandler())
-
-	log.resetError()
-	worker.start()
 	notifier.start()
+
+
+def persy_start():
+	''' Starts Persy'''
+	global worker
+	log.info("start working")
+	worker = TheSyncer(config['remote']['sleep'], config['local']['sleep'])
+	worker.start()
+	log.resetError()
+
+	notify_init()
+
+
 	if statusIcon:
 		statusIcon.set_from_file(ICON_OK)#from_stock(gtk.STOCK_HOME)
 
@@ -723,6 +743,9 @@ def gitstatus():
 
 def main(argv):
 	args = argv[1:]
+
+	#change in the userhome for all actions
+	os.chdir(USERHOME)
 
 	#cli options
 	from optparse import OptionParser
